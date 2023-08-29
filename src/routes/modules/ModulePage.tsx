6d5a3ca35f1bed4bf4178b915d1f009dac919c3c
Austin Smith <austint.smith48@gmail.com>
2023-08-28 22:24:18 -0500

    Added approval modal

--- src/routes/modules/ModulePage.tsx
+++ src/routes/modules/ModulePage.tsx
@@ -1,47 +1,65 @@
 import mockModuleData from "../../mockdata.ts";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "../../../components/ui/card.tsx";
 import {Button} from "../../../components/ui/button.tsx";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import {ImDownload} from "react-icons/im";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../../components/ui/dialog"
+import { useState } from "react";
 
 export default function ModulePage() {
-    const {id} = useParams();
+    const {id = ""} = useParams();
+    const moduleData = mockModuleData[parseInt(id) - 1];
+
+    const [openConfirmApproveModal, setOpenConfirmApproveModal] = useState(false);
+
+    // TODO: Implement approveModule to actually
+    // approve the module in the database
+    const approveModule = () => {
+        setOpenConfirmApproveModal(false);
+    };
+
     return (
         <>
             <div id="module-container" className="flex gap-4">
-                {mockModuleData.filter((module) => module.ModuleId === Number(id)).map((module) => (
-                    <div className="mb-4 w-4/5 flex flex-col gap-3">
-                        <Card>
+                <div className="mb-4 w-4/5 flex flex-col gap-3">
+                    <Card>
+                        <CardHeader>
+                            <CardDescription>{moduleData.ModuleShortDescription}</CardDescription>
+                        </CardHeader>
+                    </Card>
+                    <div className="flex flex-col gap-3">
+                        <Card key={moduleData.ModuleId}>
                             <CardHeader>
-                                <CardDescription>{module.ModuleShortDescription}</CardDescription>
+                                <CardTitle>{moduleData.ModuleName}</CardTitle>
+                                <CardDescription>{moduleData.ModuleShortDescription}</CardDescription>
                             </CardHeader>
+                            <CardContent>
+                                <div>Author ID: {moduleData.ModuleAuthorId}</div>
+                                <div>Downloads: {moduleData.ModuleDownloads}</div>
+                                <div>Created At: {moduleData.ModuleCreatedAt}</div>
+                                <div>Updated At: {moduleData.ModuleUpdatedAt}</div>
+                                <div>Markdown: {moduleData.ModuleMarkdown}</div>
+                            </CardContent>
                         </Card>
-                        <div className="flex flex-col gap-3">
-                            <Card key={module.ModuleId}>
-                                <CardHeader>
-                                    <CardTitle>{module.ModuleName}</CardTitle>
-                                    <CardDescription>{module.ModuleShortDescription}</CardDescription>
-                                </CardHeader>
-                                <CardContent>
-                                    <div>Author ID: {module.ModuleAuthorId}</div>
-                                    <div>Downloads: {module.ModuleDownloads}</div>
-                                    <div>Created At: {module.ModuleCreatedAt}</div>
-                                    <div>Updated At: {module.ModuleUpdatedAt}</div>
-                                    <div>Markdown: {module.ModuleMarkdown}</div>
-                                </CardContent>
-                            </Card>
 
-                            <Card className="">
-                                <CardHeader>
-                                    <CardTitle>Dependancies</CardTitle>
-                                </CardHeader>
-                                <CardContent>
-                                    <p>Dependencies go here</p>
-                                </CardContent>
-                            </Card>
-                        </div>
+                        <Card className="">
+                            <CardHeader>
+                                <CardTitle>Dependancies</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p>Dependencies go here</p>
+                            </CardContent>
+                        </Card>
                     </div>
-                    
-                ))}
+                </div>
                 
                 <Card className="w-1/5 flex flex-col gap-3">
                     <CardHeader>
@@ -49,8 +67,26 @@ export default function ModulePage() {
                     </CardHeader>
                     <CardContent>
                         <div className="flex flex-col gap-3">
-                            <Button>Download</Button>
-                            <Button variant="destructive">Approve</Button>
+                            <Link to={`/module/${moduleData.ModuleAuthorId}/download`}>
+                                <Button className="w-full"><ImDownload className="mr-2 h-4 w-4" />Download</Button>
+                            </Link>
+                            <Dialog open={openConfirmApproveModal} onOpenChange={setOpenConfirmApproveModal}>
+                                <DialogTrigger asChild>
+                                    <Button variant={"ghost"}>Approve</Button>
+                                </DialogTrigger>
+                                <DialogContent className="sm:max-w-[425px]">
+                                    <DialogHeader>
+                                    <DialogTitle>Are you sure?</DialogTitle>
+                                    <DialogDescription>
+                                        Are you sure you want to approve this module?
+                                    </DialogDescription>
+                                    </DialogHeader>
+                                    <DialogFooter>
+                                        <Button onClick={() => setOpenConfirmApproveModal(false)} variant={"destructive"}>Cancel</Button>
+                                        <Button onClick={() => approveModule()} type="submit">Approve</Button>
+                                    </DialogFooter>
+                                </DialogContent>
+                            </Dialog>
                         </div>
                     </CardContent>
                 </Card>
