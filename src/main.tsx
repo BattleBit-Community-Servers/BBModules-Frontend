877a928da8d4fb4fe8d1dfa946404675fb60e8f1
Austin Smith <austint.smith48@gmail.com>
2023-08-28 20:43:26 -0500

    Added logo and updated nav

--- src/main.tsx
+++ src/main.tsx
@@ -9,21 +9,39 @@ import ModuleListPage from "./routes/modules/ModuleList.tsx";
 import {BiSolidHome} from "react-icons/bi";
 import {IoExtensionPuzzle} from "react-icons/io5";
 
+const navItems = [
+    {
+        name: "Home",
+        path: "/",
+        icon: <BiSolidHome/>
+    },
+    {
+        name: "Modules",
+        path: "/modules",
+        icon: <IoExtensionPuzzle/>
+    },
+];
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <div>
-                <ul>
-                    <li className="flex items-center gap-2"><BiSolidHome/><Link to={"/"}>Home</Link></li>
-                    <li className="flex items-center gap-2"><IoExtensionPuzzle/><Link to={"/modules"}>Modules</Link></li>
-                </ul>
-            </div>
+        <div className="max-w-6xl mx-auto">
+            <nav className="flex gap-4 mb-8">
+                <Link className="flex items-center gap-2" to={"/"}><img src="/img/logo.webp" alt="Logo" width={48} height={48}/></Link>
+
+                {/* Loop through nav items */}
+                {navItems.map((item) => (
+                    <Link className="flex items-center gap-2" to={item.path}>{item.icon}{item.name}</Link>
+                ))}
+                
+            </nav>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path={"/module/:id"} element={<ModulePage/>}/>
                 <Route path={"/modules"} element={<ModuleListPage/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
+        </div>
         </BrowserRouter>
     </React.StrictMode>,
 )
