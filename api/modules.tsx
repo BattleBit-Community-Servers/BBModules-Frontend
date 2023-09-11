4eda6108bc83ccf9db1751d01beaf51348fee318
RainOrigami <rain+github@codefreak.net>
2023-09-11 12:06:05 +0200

    Change getModule(s)
    - Adds strongly typed return value
    - Removes error handling -> done by caller
    - Adds timeout to getModules

--- api/modules.tsx
+++ api/modules.tsx
@@ -1,21 +1,25 @@
+import { FilteredModuleList, ModuleData } from "./modules.types";
+
 const apiUrl = "https://apirunner.mevng.net";
 
-export async function getModules(page: number) {
-    try {
-      const response = await fetch(`${apiUrl}/Modules/GetModules?page=${page}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      return error;
+export async function getModules(page: number, timeoutMs = 5000) : Promise<FilteredModuleList> {
+    const controller = new AbortController();
+    const timeout = setTimeout(() => controller.abort(), timeoutMs);
+    
+    const response = await fetch(`${apiUrl}/Modules/GetModules?page=${page}`,
+        { signal: controller.signal });
+    
+    clearTimeout(timeout);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch module: ${response.status} ${response.statusText}`);
     }
+
+    const data = await response.json();
+    return data;
 }
 
-export async function getModule(id: number) {
-    try {
-      const response = await fetch(`${apiUrl}/Modules/GetModule/${id}`);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      return error;
-    }
+export async function getModule(id: number) : Promise<ModuleData> {
+    const response = await fetch(`${apiUrl}/Modules/GetModule/${id}`);
+    const data = await response.json();
+    return data;
 }
