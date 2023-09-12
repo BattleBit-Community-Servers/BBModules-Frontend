b7baf4256902c5704899e1b2c606e5ce2b3be528
RainOrigami <rain+github@codefreak.net>
2023-09-12 09:57:29 +0200

    Align card footer to bottom
    Author @SvenvtH

--- src/routes/modules/ModuleList.tsx
+++ src/routes/modules/ModuleList.tsx
@@ -165,7 +165,10 @@ export default function ModuleListPage() {
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
                     {modules?.results.map((module: ModuleData) => (
-                        <Card key={module.Module_id}>
+                        <Card
+                            key={module.Module_id}
+                            className="flex flex-col justify-between"
+                        >
                             <CardHeader>
                                 <CardTitle>
                                     <Link to={`/module/${module.Module_id}`}>
