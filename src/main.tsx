f88c584e2b458ee9f7f52c67d3cdbef275d90851
RainOrigami <rain+github@codefreak.net>
2023-09-15 11:53:13 +0200

    Adds cookie-based user context to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App.tsx";
+import Cookies from 'universal-cookie';
 
 import './global.css';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { UserContext } from '../api/user.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App/>
+        <UserContext.Provider value={new Cookies().get('userdata')}>
+            <App/>
+        </UserContext.Provider>
     </React.StrictMode>,
 );
