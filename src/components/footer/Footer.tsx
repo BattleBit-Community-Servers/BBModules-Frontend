6c81b2ae87c750ba8a817dfd3a079f6d63186128
DevMiner <devminer@devminer.xyz>
2023-09-20 23:02:26 +0200

    refactor: better plain link styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom";
+import { BBLink } from "../common/link.tsx";
 
 export const Footer = () => {
     return (
-        <footer className="mx-auto w-full text-center my-8">
-            This project is part of the{" "}
-            <Link to="https://battlebit.community/">battlebit.community</Link>.{" "}
-            <Link to={"/privacy"}>Privacy Policy</Link>
+        <footer className="mx-auto my-8 flex w-full items-center justify-center gap-3">
+            <div className="flex-1 text-end">
+                This project is part of the <BBLink to="https://battlebit.community/">battlebit.community</BBLink>
+            </div>
+
+            <div className="h-1 w-1 rounded-full bg-muted"></div>
+
+            <div className="flex-1">
+                <BBLink to={"/privacy"}>Privacy Policy</BBLink>
+            </div>
         </footer>
     );
 };
